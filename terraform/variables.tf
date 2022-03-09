f8f97c86878168ce936a744ed5a9cd2b7ac8d386
Josie Lander <landerjosie@gmail.com>
2022-03-09 15:37:05 +0000

    k8s terraform config

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "k8s_cluster_name" {
+    type = string
+}
+
+variable "resource_group_name" {
+    type = string
+}
+
+variable "node_resource_group" {
+    type = string
+}
+
+variable "admin_username" {
+    type = string
+}
+
+variable "vm_size" {
+    type = string
+}
+
+variable "tags" {
+    
+}
